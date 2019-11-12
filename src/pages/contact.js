e12c22b91240ef8635814233715197de7e964c41
andremcd <andredemcd@hotmail.com>
2019-11-12 18:34:30 -0300

    make news pages

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+    <Layout>
+        <SEO title="contact" />
+        <h1>Contact Page</h1>
+    </Layout>
+);
+
+export default ContactPage;
