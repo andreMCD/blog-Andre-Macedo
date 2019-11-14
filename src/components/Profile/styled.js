488f6dda506305d9dc72eaf6b64eb431be10bd78
andremcd <andredemcd@hotmail.com>
2019-11-14 04:23:23 -0300

    modify styles

--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,10 +1,30 @@
 import styled from 'styled-components'
 
 export const ProfileWrapper = styled.section`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    width: 100%;
+    padding: 2.5rem 0 0;
+    border: 1px solid wheat;
+`
+export const ProfileTitle = styled.h1`
+    font-size: 4rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #393e46;
+    margin: 2rem 0 .5rem 0;
+`
+
+export const ProfileSubTitle = styled.h2`
+    font-size: 2rem;
+    color: #1d2029;
+    margin-bottom: 2rem;
+    border-bottom: 1px solid #00adb5;
+`
+
+export const ProfileDescription = styled.p`
+    font-size: 1.2rem;
+    color: #393e46;
 
 `
-export const ProfileTitle = styled.h1``
-
-export const ProfileSubTitle = styled.h2``
-
-export const ProfileDescription = styled.p``
