0e7683fa3e3570418f3c0d7494b3b82f5ad89b12
andremcd <andredemcd@hotmail.com>
2019-11-14 05:37:29 -0300

    create the component "social-icons" but incomplete

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/andreMCD',
+    },
+    {
+        label: 'Linkedin',
+        url: 'https://linkedin.com/in/andremcd',
+    },
+    {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/andredemcd/',
+    },
+    {
+        label: 'Facebook',
+        url: '',
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/andredeMCD',
+    }
+]
+
+export default links
