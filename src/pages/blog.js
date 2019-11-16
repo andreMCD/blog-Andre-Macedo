e854679fd5b9bb51fda80d81b2ac55c7082371df
andremcd <andredemcd@hotmail.com>
2019-11-16 20:22:02 -0300

    import of the PostItem component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react"
+
+import PostItem from "../components/PostItem"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const BlogPage = () => (
-    <Layout>
-        <SEO title="blog" />
-        <h1>Contact Page</h1>
-    </Layout>
-);
+  <Layout>
+    <SEO title="blog" />
+    <PostItem
+      border="#00f911"
+      slug="/about/"
+      title="Título do Post"
+      date="30 de Julho de 2019"
+      timeToRead="7"
+      description="Essa é uma pequena descrição do post."
+    />
+  </Layout>
+)
 
-export default BlogPage;
+export default BlogPage
